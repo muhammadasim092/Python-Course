48c2f67bac4fc736be889655f7599f66bfa6efc9
Muhammad Asim <97046562+muhammadasim092@users.noreply.github.com>
2025-06-29 21:41:46 +0500

    inheritance

--- Python Course/inheritance.py
+++ Python Course/inheritance.py
@@ -1,1 +1,17 @@
-print("Hello")
+class Employe:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+    
+    def showDetails(self):
+        print(f"The name of Employee: {self.id} is {self.name}")
+
+class Manager(Employe):
+    def showLanguage(self):
+        print("The language of Manager is Python")
+        
+        
+e = Employe("Asim", 123)
+e.showDetails() 
+e2 = Manager("Asim" , 1234 )
+e2.showLanguage()
